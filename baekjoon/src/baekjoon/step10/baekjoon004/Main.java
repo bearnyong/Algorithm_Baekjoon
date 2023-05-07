fb09c1bec011c2df19435e9afd05f71fab534ad0
bearnyong <rhalsdud216@gmail.com>
2023-05-07 21:50:27 +0900

    test

--- baekjoon/src/baekjoon/step10/baekjoon004/Main.java
+++ baekjoon/src/baekjoon/step10/baekjoon004/Main.java
@@ -1,9 +1,15 @@
 package baekjoon.step10.baekjoon004;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		long rec = sc.nextLong();
+		System.out.println(rec*4);
 
 	}
 
