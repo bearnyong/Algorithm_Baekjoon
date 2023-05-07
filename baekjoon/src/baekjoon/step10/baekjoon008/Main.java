a5cfac042c0ba8640405d9e265b74e83a557e59a
bearnyong <rhalsdud216@gmail.com>
2023-05-08 01:13:36 +0900

    baekjoontest

--- baekjoon/src/baekjoon/step10/baekjoon008/Main.java
+++ baekjoon/src/baekjoon/step10/baekjoon008/Main.java
@@ -1,5 +1,6 @@
 package baekjoon.step10.baekjoon008;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -14,8 +15,16 @@ public class Main {
 	    	  stick[i] = sc.nextInt();
 	    }
 	    
+	    Arrays.sort(stick); // Arrays.sort()로 입력받은 stick 값 오름차순 정렬
 	    
-		
+	    // 삼각형이 되기 위한 조건 : (a+b) > c
+	    if((stick[0] + stick[1]) > stick[2]) {
+	        System.out.println(stick[0] + stick[1] + stick[2]); 
+	    } else {
+	    	stick[2] = (stick[0] + stick[1])-1;
+	    	System.out.println(stick[0] + stick[1] + stick[2]);
+	    }
+	    
 	}
 
 }
