7b6b077eea371516580fbc17ea9ee01a2942f916
bearnyong <rhalsdud216@gmail.com>
2023-05-08 04:19:44 +0900

    baekjoontest

--- baekjoon/src/baekjoon/step08/baekjoon001/Main.java
+++ baekjoon/src/baekjoon/step08/baekjoon001/Main.java
@@ -11,7 +11,13 @@ public class Main {
 		int num = sc.nextInt();
 		int bub = sc.nextInt();
 		
+		String num2 = String.valueOf(num); //숫자의 개별 자릿수 얻는 방법
+		char[] num3 = num2.toCharArray();  //숫자의 개별 자릿수 얻는 방법
 		
+		
+		for(int i=1; i<=num3.length; i++) {
+			Math.pow(num3[i-1], i);
+		}
 
 	}
 
