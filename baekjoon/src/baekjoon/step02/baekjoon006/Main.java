c9e72176738bb8cba3c27a8bc972a6677ec96bcc
bearnyong <rhalsdud216@gmail.com>
2023-05-14 13:35:26 +0900

    baekjoontest

--- baekjoon/src/baekjoon/step02/baekjoon006/Main.java
+++ baekjoon/src/baekjoon/step02/baekjoon006/Main.java
@@ -1,9 +1,29 @@
 package baekjoon.step02.baekjoon006;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int h = sc.nextInt();
+		int m1 = sc.nextInt();
+		
+		int m2 = sc.nextInt();
+		
+		if((m1+m2)/60 == 0) {
+			m1 += m2;
+			System.out.println(h+" "+m1);
+		} else {
+			h += (m1+m2)/60;
+			m1 = (m1+m2)%60;
+			if(h>23) {
+				h-=24;
+			}
+			System.out.println(h+" "+m1);
+		}
+		
 
 	}
 
